9149000d1bf670f9bb31fbea04051ae0382292f2
JVFCN <j3280891657@qq.com>
2023-07-12 11:11:42 +0800

    SetBoard

--- yunhu/openapi.py
+++ yunhu/openapi.py
@@ -1,4 +1,3 @@
-
 import json
 import requests
 
@@ -12,66 +11,118 @@ class Openapi(object):
     def __init__(self, token: str) -> None:
         self.token = token
 
-    def sendMessage(self, recvId: str, recvType: str, content: map):
-        """
-        单条，发送文本消息
-        """
-        return self.sendMessage(recvId, recvType, "text", content)
+    class Messages:
+        def __init__(self, outer_instance):
+            self.outer_instance = outer_instance
 
-    def sendMarkdownMessage(self, recvId: str, recvType: str, content: map):
-        """
-        单条，发送markdown消息
-        """
-        return self.sendMessage(recvId, recvType, "markdown", content)
-    
-    def sendMessage(self, recvId: str, recvType: str, contentType: str, content: map):
-        """
-        单条，发送单条消息
-        """
-        params = {
-            "recvId": recvId, 
-            "recvType": recvType, 
-            "contentType": contentType, 
-            "content": content
-         }
-        headers = {'Content-Type': 'application/json'}
-        return requests.post(self.baseUrl + '/bot/send?token=' + self.token,headers=headers, data=json.dumps(params))
+        def sendTextMessage(self, recvId: str, recvType: str, content: map):
+            """
+            单条，发送文本消息
+            """
+            return self.sendMessage(recvId, recvType, "text", content)
 
-    def batchSendTextMessage(self, recvIds: list, recvType: str, content: map):
-        """
-        批量，发送文本消息
-        """
-        return self.batchSendMessage(recvIds, recvType, "text", content)
+        def sendMarkdownMessage(self, recvId: str, recvType: str, content: map):
+            """
+            单条，发送markdown消息
+            """
+            return self.sendMessage(recvId, recvType, "markdown", content)
 
-    def batchSendMarkdownMessage(self, recvIds: list, recvType: str, content: map):
-        """
-        批量，发送markdown消息
-        """
-        return self.batchSendMessage(recvIds, recvType, "markdown", content)
-    
-    def batchSendMessage(self, recvIds: list, recvType: str, contentType: str, content: map):
-        """
-        批量，批量发送消息
-        """
-        params = {
-            "recvIds": recvIds, 
-            "recvType": recvType, 
-            "contentType": contentType, 
-            "content": content
-         }
-        headers = {'Content-Type': 'application/json'}
-        return requests.post(self.baseUrl + '/bot/batch_send?token=' + self.token,headers=headers, data=json.dumps(params))
-    
-    def editMessage(self, msgId: str, recvId: str, recvType: str, contentType: str, content: map):
-        """
-        单条，编辑单条消息
-        """
-        params = {
-            "msgId": msgId, 
-            "recvId": recvId, 
-            "recvType": recvType, 
-            "contentType": contentType, 
-            "content": content
-         }
-        headers = {'Content-Type': 'application/json'}
-        return requests.post(self.baseUrl + '/bot/edit?token=' + self.token,headers=headers, data=json.dumps(params))
+        def sendMessage(self, recvId: str, recvType: str, contentType: str, content: dict):
+            """
+            单条，发送单条消息
+            """
+            params = {
+                "recvId": recvId,
+                "recvType": recvType,
+                "contentType": contentType,
+                "content": content
+            }
+            headers = {'Content-Type': 'application/json'}
+            return requests.post(f"{Openapi.baseUrl}/bot/send?token={self.outer_instance.token}", headers=headers,
+                                 data=json.dumps(params))
+
+        def batchSendTextMessage(self, recvIds: list, recvType: str, content: dict):
+            """
+            批量，发送文本消息
+            """
+            return self.batchSendMessage(recvIds, recvType, "text", content)
+
+        def batchSendMarkdownMessage(self, recvIds: list, recvType: str, content: dict):
+            """
+            批量，发送markdown消息
+            """
+            return self.batchSendMessage(recvIds, recvType, "markdown", content)
+
+        def batchSendMessage(self, recvIds: list, recvType: str, contentType: str, content: dict):
+            """
+            批量，批量发送消息
+            """
+            params = {
+                "recvIds": recvIds,
+                "recvType": recvType,
+                "contentType": contentType,
+                "content": content
+            }
+            headers = {'Content-Type': 'application/json'}
+            return requests.post(f"{Openapi.baseUrl}/bot/batch_send?token={self.outer_instance.token}", headers=headers,
+                                 data=json.dumps(params))
+
+        def editMessage(self, msgId: str, recvId: str, recvType: str, contentType: str, content: dict):
+            """
+            单条，编辑单条消息
+            """
+            params = {
+                "msgId": msgId,
+                "recvId": recvId,
+                "recvType": recvType,
+                "contentType": contentType,
+                "content": content
+            }
+            headers = {'Content-Type': 'application/json'}
+            return requests.post(f"{Openapi.baseUrl}/bot/edit`?token={self.outer_instance.token}", headers=headers,
+                                 data=json.dumps(params))
+
+    class Board:
+        def __init__(self, outer_instance):
+            self.outer_instance = outer_instance
+
+        def setBoard(self, recvId: str, recvType: str, contentType: str, content: str):
+            """
+            设置某用户的看板内容
+            """
+            params = {
+                "recvId": recvId,
+                "recvType": recvType,
+                "contentType": contentType,
+                "content": content
+            }
+            headers = {'Content-Type': 'application/json; charset=utf-8'}
+            requests.post(f"{Openapi.baseUrl}/bot/board?token={self.outer_instance.token}", headers=headers,
+                          data=json.dumps(params))
+
+        def setAllUserBoard(self, contentType: str, content: str):
+            """
+            设置所有用户的看板内容
+            """
+            params = {
+                "contentType": contentType,
+                "content": content
+            }
+            headers = {'Content-Type': 'application/json; charset=utf-8'}
+            requests.post(f"{Openapi.baseUrl}/bot/board-all?token={self.outer_instance.token}", headers=headers,
+                          data=json.dumps(params))
+
+        def CancelBoard(self, recvId: str, recvType: str):
+            """
+            取消用户的看板
+            """
+            params = {
+                "recvId": recvId,
+                "recvType": recvType
+            }
+            headers = {'Content-Type': 'application/json; charset=utf-8'}
+            requests.post(f"{Openapi.baseUrl}/bot/board-dismiss?token={self.outer_instance.token}", headers=headers,
+                          data=json.dumps(params))
+
+        def CancelAllUserBoard(self):
+            requests.post(f"{Openapi.baseUrl}/bot/board-all-dismiss?token={self.outer_instance.token}")
