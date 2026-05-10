33cc8bc3970ebe4c7a5822736031a76409ac3e7d
ninewan <ninesheng@gmail.com>
2026-05-10 12:14:32 +0800

    0.0.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
  
 setuptools.setup(
     name="yunhu", 
-    version="0.0.11", 
+    version="0.0.12",
     author="yunhu",  
     description="云湖官方Python SDK",
     author_email="support@jwzhd.com",
